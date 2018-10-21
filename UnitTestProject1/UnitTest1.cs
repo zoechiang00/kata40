1fb1c6ca9cbcfba2c0e0a083cc5fba9d8b266d48
TITANSOFT\zoe.chiang85 <zoe.chiang@titansoft.com.sg>
2018-10-21 18:10:13 +0800

    no message

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using kata40;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestsEscaped()
+        {
+            Assert.AreEqual("Escaped!", Program.CatMouse("C....m"));
+        }
+
+        [TestMethod]
+        public void TestsCaught()
+        {
+            Assert.AreEqual("Caught!", Program.CatMouse("C..m"));
+        }
+    }
+}
---
+++ kata40/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata40
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+        public static string CatMouse(string x)
+        {
+            // Your code here...
+            var characterslength = x.ToCharArray().Length - 2;
+            return characterslength > 3 ? "Escaped!" : "Caught!";
+        }
+    }
+}
